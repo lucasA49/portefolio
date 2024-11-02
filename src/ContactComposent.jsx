cc9ec7bef7ed7550682fd84bf14e1788925e6014
lucasA49 <lucasaksu@gmail.com>
2024-11-02 02:19:32 +0100

    mise en place de la page contact

---
+++ src/ContactComposent.jsx
@@ -0,0 +1,8 @@
+function ContactComposant () {
+
+<h1>HELLO world</h1>
+
+
+}
+
+export default  ContactComposant;
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,12 +1,13 @@
 import Background from "../Background";
 import ButtonNav from "../ButtonNav";
+import ContactComposant from "../ContactComposent"
 
 function Contact () {
 
     <>
         <Background />
         <ButtonNav />
-
+        <ContactComposant/>
     </>
 }
  
