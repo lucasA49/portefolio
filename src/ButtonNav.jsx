2d37d66c22b784abe41da5fbd85086b200a2dc1f
lucasA49 <lucasaksu@gmail.com>
2024-11-02 01:29:47 +0100

    Mise en place du logo de la navbar

--- src/ButtonNav.jsx
+++ src/ButtonNav.jsx
@@ -1,11 +1,11 @@
 import "./btn-nav.css";
 import { NavLink } from "react-router-dom";
-
+import Home from "./assets/Home.png"
 function ButtonNav() {
     return (
         <section className="section-btn">
             <NavLink
-                to="/projet" 
+                to="/projet"
                 className="a-btn"
             >
                 <div className="btn">
@@ -17,6 +17,11 @@ function ButtonNav() {
                     />
                 </div>
             </NavLink>
+            <NavLink to="/" className="">
+    <img src={Home} className="logo-home" alt="Accueil" />
+</NavLink>
+
+        
             <NavLink
                 to="/about"
                 className="a-btn"
